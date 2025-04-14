ee5043d4ea2b17a6593c93764d97f7a9d2f27b40
sheheryar <sherikhan451@gmail.com>
2025-04-15 04:38:13 +0500

    BackUp Model

---
+++ SecurityServices/Models/BackupModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SecurityServices.Models
+{
+    public class BackupModel
+    {
+        //public class Employee { ... } // as you already have
+        //public class Vacancy { public int Id; public string Title; public string Description; public bool IsFilled; ... }
+        //public class Client { public int Id; public string Name; public string ServicesAvailed; public string StaffAssigned; }
+        //public class Testimonial { public int Id; public string Name; public string Content; public DateTime DatePosted; }
+        //public class Branch { public int Id; public string Region; public string Address; public string ContactPerson; public string Phone; }
+        //public class Service { public int Id; public string Name; public string Description; }
+
+    }
+}
