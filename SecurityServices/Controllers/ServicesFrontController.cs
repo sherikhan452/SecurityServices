e6e3c1e93ec9226fe23bf55bf85714f14dd409f9
sheheryar <sherikhan451@gmail.com>
2025-04-17 04:29:55 +0500

    FrontController

---
+++ SecurityServices/Controllers/ServicesFrontController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecurityServices.Data;
+using SecurityServices.Models;
+
+namespace SecurityServices.Controllers
+{
+    public class ServicesFrontController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public ServicesFrontController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ServicesFront
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Services.ToListAsync());
+        }
+
+        // GET: ServicesFront/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // GET: ServicesFront/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ServicesFront/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ServiceId,Name,Description")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: ServicesFront/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+            return View(service);
+        }
+
+        // POST: ServicesFront/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ServiceId,Name,Description")] Service service)
+        {
+            if (id != service.ServiceId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(service);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ServiceExists(service.ServiceId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+
+        // GET: ServicesFront/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var service = await _context.Services
+                .FirstOrDefaultAsync(m => m.ServiceId == id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return View(service);
+        }
+
+        // POST: ServicesFront/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var service = await _context.Services.FindAsync(id);
+            if (service != null)
+            {
+                _context.Services.Remove(service);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return _context.Services.Any(e => e.ServiceId == id);
+        }
+    }
+}
---
+++ SecurityServices/Controllers/VacanciesFrontController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecurityServices.Data;
+using SecurityServices.Models;
+
+namespace SecurityServices.Controllers
+{
+    public class VacanciesFrontController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public VacanciesFrontController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VacanciesFront
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vacancy.ToListAsync());
+        }
+
+        // GET: VacanciesFront/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacancy = await _context.Vacancy
+                .FirstOrDefaultAsync(m => m.VacancyId == id);
+            if (vacancy == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacancy);
+        }
+
+        // GET: VacanciesFront/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VacanciesFront/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("VacancyId,Position,Description,IsFilled")] Vacancy vacancy)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vacancy);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vacancy);
+        }
+
+        // GET: VacanciesFront/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacancy = await _context.Vacancy.FindAsync(id);
+            if (vacancy == null)
+            {
+                return NotFound();
+            }
+            return View(vacancy);
+        }
+
+        // POST: VacanciesFront/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("VacancyId,Position,Description,IsFilled")] Vacancy vacancy)
+        {
+            if (id != vacancy.VacancyId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vacancy);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VacancyExists(vacancy.VacancyId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vacancy);
+        }
+
+        // GET: VacanciesFront/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vacancy = await _context.Vacancy
+                .FirstOrDefaultAsync(m => m.VacancyId == id);
+            if (vacancy == null)
+            {
+                return NotFound();
+            }
+
+            return View(vacancy);
+        }
+
+        // POST: VacanciesFront/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vacancy = await _context.Vacancy.FindAsync(id);
+            if (vacancy != null)
+            {
+                _context.Vacancy.Remove(vacancy);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VacancyExists(int id)
+        {
+            return _context.Vacancy.Any(e => e.VacancyId == id);
+        }
+    }
+}
