d516cbb1052e53d45885780c2f8b50d866f9faa8
sheheryar <sherikhan451@gmail.com>
2025-04-20 20:33:51 +0500

    jh

---
+++ SecurityServices/Controllers/RegionsNetworkController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SecurityServices.Data;
+using SecurityServices.Models;
+
+namespace SecurityServices.Controllers
+{
+    public class RegionsNetworkController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public RegionsNetworkController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RegionsNetwork
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Regions.ToListAsync());
+        }
+
+        // GET: RegionsNetwork/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions
+                .FirstOrDefaultAsync(m => m.RegionId == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return View(region);
+        }
+
+        // GET: RegionsNetwork/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RegionsNetwork/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RegionId,Name,Address,ContactDetails,ContactPerson")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(region);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(region);
+        }
+
+        // GET: RegionsNetwork/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions.FindAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            return View(region);
+        }
+
+        // POST: RegionsNetwork/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RegionId,Name,Address,ContactDetails,ContactPerson")] Region region)
+        {
+            if (id != region.RegionId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(region);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RegionExists(region.RegionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(region);
+        }
+
+        // GET: RegionsNetwork/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var region = await _context.Regions
+                .FirstOrDefaultAsync(m => m.RegionId == id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return View(region);
+        }
+
+        // POST: RegionsNetwork/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+            if (region != null)
+            {
+                _context.Regions.Remove(region);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RegionExists(int id)
+        {
+            return _context.Regions.Any(e => e.RegionId == id);
+        }
+    }
+}
